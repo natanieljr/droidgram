a2066c7c4942dbaf73bb24fd980c325c06ae2ae5
natanieljr <nataniel.borges@cispa.saarland>
2019-05-29 16:27:57 +0200

    Inherit from abstract class instead of ExplorationStrategy
    Prevent `cannotMoveForward` errors caused by an assert in the class

--- src/main/kotlin/org/droidmate/droidgram/exploration/GrammarStrategy.kt
+++ src/main/kotlin/org/droidmate/droidgram/exploration/GrammarStrategy.kt
@@ -1,16 +1,17 @@
 package org.droidmate.droidgram.exploration
 
 import org.droidmate.deviceInterface.exploration.ExplorationAction
-import org.droidmate.exploration.strategy.widget.ExplorationStrategy
+import org.droidmate.exploration.strategy.AbstractStrategy
+import java.util.UUID
 
-class GrammarStrategy(generatedInput: String, grammarMapping: Map<String, String>) : ExplorationStrategy() {
+class GrammarStrategy(generatedInput: String, grammarMapping: Map<String, UUID>) : AbstractStrategy() {
     internal val grammarWatcher by lazy {
         (eContext.findWatcher { it is GrammarReplayMF }
             ?: GrammarReplayMF(generatedInput, grammarMapping)
                 .also { eContext.addWatcher(it) }) as GrammarReplayMF
     }
 
-    override suspend fun chooseAction(): ExplorationAction {
+    override suspend fun internalDecide(): ExplorationAction {
         return grammarWatcher.nextAction(currentState, true)
     }
 }

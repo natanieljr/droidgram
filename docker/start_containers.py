e10f0721f23c26960373a91ffaba8d4fe55bc3a2
natanieljr <nataniel.borges@cispa.saarland>
2019-07-13 09:04:24 +0200

    Updated to start rq2

--- docker/start_containers.py
+++ docker/start_containers.py
@@ -5,7 +5,7 @@ d = "/home/nataniel.borges/droidgram/docker/experiments/"
 dirs = [join(d, f) for f in os.listdir(d)]
 
 for d in filter(lambda x: os.path.isdir(x), dirs):
-    c = 'docker run --privileged -d -v %s:/test/experiment --name nataniel.%s droidgram bash -c "./runAll.sh"' % (d, basename(d))
+    c = 'docker run --privileged -d -v %s:/test/experiment --name nataniel.%s droidgram bash -c "./runExperimentRQ2.sh"' % (d, basename(d))
     print(c)
     os.system(c)
 
